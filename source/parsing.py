9bfc69097f55c0252bc9680242258b1089832c3e
Anna Leoshko <annaleoshko1@gmail.com>
2020-04-08 18:05:54 +0300

    add to db added

--- source/parsing.py
+++ source/parsing.py
@@ -3,14 +3,17 @@ import googleapiclient.discovery
 from tqdm import tqdm
 from functions import get_top_level_comments, get_child_comments_by_parent_id
 from settings import DEVELOPER_KEY
+from db_settings import session
 import re
 from sentiment_analysis import text_analysis
+import time
+from settings import api_service_name, api_version
+from models import Comment
+
 
 def get_all_comments(video_id):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
-    api_service_name = "youtube"
-    api_version = "v3"
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
@@ -18,6 +21,8 @@ def get_all_comments(video_id):
     top_level_comments = []
     current_page_comments, next_page_token = get_top_level_comments(youtube, video_id)
     top_level_comments += current_page_comments
+
+    start = time.time()
 
     while next_page_token:
         current_page_comments, next_page_token = get_top_level_comments(youtube, video_id, next_page_token)
@@ -31,7 +36,9 @@ def get_all_comments(video_id):
     
     all_comments = text_analysis(all_comments)
 
-    print(all_comments)
+    end = time.time()
+
+    print('Время выполнения функции: ', end - start)
     
     return all_comments
 
@@ -42,3 +49,22 @@ def get_video_id(url):
         return match.group(1)
     else:
         return None
+
+def add_to_db (all_comments):
+    session.query(Comment).delete()
+    session.commit()
+
+    for comment in all_comments:
+        c = Comment(
+            video_id=comment['video_id'],
+            comment_id=comment['comment_id'],
+            author_name=comment['author_name'],
+            url=comment['url'],
+            comment=comment['comment'], 
+            date=comment['date'],
+            like_count=comment['like_count']
+        )
+        session.add(c)
+    session.commit()
+
+add_to_db(all_comments)
